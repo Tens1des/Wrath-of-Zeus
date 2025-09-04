4890d343abcf3a74d40097be66cc986e398e140d
Tens1des <romankotov3213@gmail.com>
2025-09-04 22:56:00 +0300

    Initial Commit

---
+++ Wrath of Zeus/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Wrath of Zeus
+//
+//  Created by Рома Котов on 04.09.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Wrath of Zeus/Wrath_of_ZeusApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wrath_of_ZeusApp.swift
+//  Wrath of Zeus
+//
+//  Created by Рома Котов on 04.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct Wrath_of_ZeusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
